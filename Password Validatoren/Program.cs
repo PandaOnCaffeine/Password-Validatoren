21ccce3beffda35fdae2e1c6d48d3f61f26c4e14
TeaPandaa <CaptPandaa@gmail.com>
2022-08-12 16:24:29 +0200

    Comments

--- Password Validatoren/Program.cs
+++ Password Validatoren/Program.cs
@@ -11,45 +11,72 @@
 
         static void Gui()
         {
+            
             Console.WriteLine("\r\nIndtast dit password");
             Program.Controller();
+            //Checks if the password are within the min and max amount of chars
+            //if false, then it gives the user a chance to write a new password by running Gui again
             if (!MinMaxChar(password))
             {
                 Console.WriteLine("Passwordet skal være på min 12 og max 64 tegn");
                 Program.Gui();
             }
+            //checks for uppercase and lowercase
             else if (!UpperLower(password))
             {
                 Console.WriteLine("Passwordet skal indeholde både store og små tegn");
                 Program.Gui();
             }
+            //Checks for numbers
             else if (!Number(password))
             {
                 Console.WriteLine("Passwordet skal indeholde numre");
                 Program.Gui();
             }
+            //Checks for special chars
             else if (!SpecialChars(password))
             {
                 Console.WriteLine("Passwordet skal indeholde et eller flere special tegn");
                 Program.Gui();
             }
+            //Checks how strong the password is
             else
             {
-                if (true)
+                //The password is not strong enough so it runs the Gui again
+                if (Has4InARow(password) && NumbersInASeq(password))
                 {
-
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Passwordet er ikke stærkt nok");
+                    Console.ResetColor();
+                    Program.Gui();
+                }
+                //The password is ok but still weak
+                else if (Has4InARow(password) || NumbersInASeq(password))
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Passwordet er OK, men betragtes som svagt");
+                    Console.ResetColor();
+                }
+                //the password is ok
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Passwordet er OK");
+                    Console.ResetColor();
                 }
             }
         }
 
         static void Moddellayer()
         {
+            //sets password to be what the user inputs
             password = Console.ReadLine();
         }
 
         static void Controller()
         {
             Program.Moddellayer();
+            //run the different bools to check the password requirements are met
             Program.MinMaxChar(password);
             Program.UpperLower(password);
             Program.Number(password);
@@ -57,7 +84,8 @@
         }
         static bool MinMaxChar(string password)
         {
-            if (password.Length > 11)
+            //checks if the chars in the password is between 12 and 64
+            if (password.Length > 11 && password.Length < 65)
             {
                 return true;
             }
@@ -68,6 +96,7 @@
         }
         static bool UpperLower(string password)
         {
+            //Checks for uppercase and lowercase chars with a foreach loop to loop through all chars in the password string
             bool upper = false;
             bool lower = false;
             foreach (char c in password)
@@ -81,6 +110,7 @@
                     lower = true;
                 }
             }
+            //returns true if theres both lower and uppercase chars
             if (upper == true && lower == true)
             {
                 return true;
@@ -92,6 +122,7 @@
         }
         static bool Number(string password)
         {
+            //checks for numbers through a foreach loop, returns true if there's a number
             foreach (char c in password)
             {
                 if (char.IsDigit(c))
@@ -103,10 +134,12 @@
         }
         static bool SpecialChars(string password)
         {
+            //checks for any char that isn't a letter or digit, returns true if there is one
             return password.Any(ch => !Char.IsLetterOrDigit(ch));
         }
         static bool Has4InARow(string password)
         {
+            //uses a for loop to check if there't a point in the string with the same char 4 times in a row, returns true if there is
             for (int i = 0; i < password.Length-3; i++)
             {
                 if (password[i] == password[i+1] && password[i+1] == password[i+2] && password[i+2] == password[i+3])
@@ -118,6 +151,7 @@
         }
         static bool NumbersInASeq(string password)
         {
+            //checks make a char array with all the chars in the string password, convert from char array to int array and changes letters to -2
             char[] arr = password.ToArray();
             int[] ints = new int[arr.Length];
             int count = 0;
@@ -135,10 +169,10 @@
                 count++;
             }
 
-
-            for (int i = 0; i < ints.Length-3; i++)
+            //checks if there's a point where number are in a acending order in a row, returns true if there is
+            for (int i = 0; i < ints.Length - 3; i++)
             {
-                if (ints[i] == ints[i + 1]+1 && ints[i + 1] == ints[i + 2]+1 && ints[i + 2] == ints[i + 3]+1)
+                if (ints[i] + 1 == ints[i + 1] && ints[i + 1] + 1 == ints[i + 2] && ints[i + 2] + 1 == ints[i + 3])
                 {
                     return true;
                 }
